35bd697a7568a0cbd4a354d92cb9cb8bbe43afe7
Alex <avask.work@gmail.com>
2022-01-01 17:02:35 +0300

    Added some more functionality to cx_array

--- constexpr_types.hpp
+++ constexpr_types.hpp
@@ -3,6 +3,8 @@
 #include <cstddef> // std::size_t
 #include <initializer_list>
 
+#include "range.hpp"
+
 namespace core {
 
 /**
@@ -32,22 +34,149 @@ constexpr auto cx_make_optional(const T& value) {
 }
 
 
+
+/**
+ * @brief constexpr array type
+ *        the array itself is an immutable compile-time evaluated type,
+ *        all the work is done returning new - modified - arrays.
+ * 
+ * @tparam T - type of elements
+ * @tparam N - size of array
+ */
 template <typename T, std::size_t N>
 class cx_array {
 public:
     constexpr cx_array (std::initializer_list<T> list) {
         size_t i=0;
         for (auto v : list) {
-            data[i++] = std::move(v);
+            data[i++] = v;
         }
     }
 
+
+    template <size_t M>
+    constexpr cx_array (cx_array<T,M> const& other) {
+        for (auto i : range(N)) {
+            data[i] = other[i];
+        }
+    }
+
+
+    constexpr cx_array (T const* other) {
+        for (auto i : range(N)) {
+            data[i] = other[i];
+        }
+    }
+
+    
+    /**
+     * @brief Initializing by concatenating the other two cx_arrays
+     * 
+     * @tparam M - size of the first array
+     * @tparam L - size of the second array
+     * @invariant M+L == N
+     */
+    template <size_t M, size_t L, typename=std::enable_if_t<(M+L == N)>>
+    constexpr cx_array (cx_array<T,M> const& a, cx_array<T,L> const& b) {
+        static_assert(M+L == N, 
+        "the result of concatenation of two arrays should have size exaclty equal to the sum of their sizes");
+        size_t i=0;
+        for (auto && v : a) {
+            data[i++] = v;
+        }
+        for (auto && w : b) {
+            data[i++] = w;
+        }
+    }
+
+
+    template <size_t M, size_t L>
+    constexpr cx_array (cx_array<T,M> && a, cx_array<T,L> && b) {
+        static_assert(M+L == N, 
+        "the result of concatenation of two arrays should have size exaclty equal to the sum of their sizes");
+        size_t i=0;
+        for (auto && v : a) {
+            data[i++] = std::move(v);
+        }
+        for (auto && w : b) {
+            data[i++] = std::move(w);
+        }
+    }
+
+
+    /**
+     * @brief Slicing returns a new array == old[From : To]
+     * 
+     * @tparam From 
+     * @tparam To 
+     * @return cx_array<T, To-From> 
+     */
+    template <size_t From, size_t To>
+    constexpr auto slice() const -> cx_array<T, To-From> {
+        static_assert(0 <= From && From <= N && 0 <= To && To <= N && To > From ,"Slice out of bounds of the array");
+        return {&data[From]};
+
+    }
+
+    /**
+     * @brief Indexing array
+     * 
+     * @tparam Idx 
+     * @return T 
+     */
+    template <size_t Idx>
+    constexpr auto at() const -> T {
+        static_assert(0<= Idx && Idx < N, "cx_array::operator[]: Index `Idx` out of range");
+        return data[Idx];
+    }
+
+    constexpr auto operator[] (size_t i) const -> T {
+        return data[i];
+    }
+
+    constexpr auto size() const -> size_t { return N; }
+
+
+    struct value_with_position {
+        T value;
+        size_t at;
+
+        constexpr value_with_position (T val, size_t pos) : value{val}, at{pos} {}
+        constexpr operator T() const { return value; }
+    };
+
+    constexpr auto max() {
+        auto res = value_with_position(data[0], 0);
+        for (auto i : range(N)) {
+            if (data[i] > res) res = {data[i], i};
+        }
+        return res;
+    }
+
+
+    constexpr auto min() {
+        auto res = value_with_position(data[0], 0);
+        for (auto i : range(N)) {
+            if (data[i] < res) res = {data[i], i};
+        }
+        return res;
+    }
+
+
+    constexpr auto find(T value) -> cx_optional<size_t> {
+        for (auto i : range(N)) {
+            if (data[i] == value) {
+                return {i};
+            }
+        }
+        return {};
+    }
 
     // Iterators
     constexpr auto begin() const noexcept { return &data[0]; }
     constexpr auto end()   const noexcept { return &data[N]; }
 
-
+    // Printing
     friend auto operator<< (std::ostream& os, cx_array array) -> std::ostream& {
         os << "[";
         for (auto& v : array) {
@@ -60,6 +189,12 @@ public:
 private:
     T data[N];
 };
+
+template <typename T, size_t N, size_t M>
+inline
+constexpr auto operator+ (cx_array<T,N> const& a, cx_array<T,M> const& b) -> cx_array<T, N+M> {
+    return {a, b};
+}
 
 
 }//namespace core
