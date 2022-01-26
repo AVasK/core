96552f3a1e50050c50ac81858689cf4145cd46d1
Alex <avask.work@gmail.com>
2022-01-26 16:44:14 +0300

    Moved class MaybeEmpty (EmptyBaseOptimization) into separate header

--- pointers.hpp
+++ pointers.hpp
@@ -4,6 +4,7 @@
 
 #include "typesystem.hpp"
 #include "macros.hpp"
+#include "maybe_empty.hpp"
 
 namespace core {
 
@@ -33,47 +34,6 @@ public:
 protected:
     ptr_type p;
 };
-
-
-
-// #if (__has_cpp_attribute(no_unique_address) || __cplusplus/100 >= 2020) // Clang
-#if CORE_HAS_ATTR(NO_UNIQUE_ADDRESS)
-#   warning [[no_unique_address]] feature is used
-template <class Base>
-class MaybeEmpty {
-public:
-    // [[no_unique_address]] Base elem;
-    NO_UNIQUE_ADDRESS Base elem;
-    constexpr MaybeEmpty(Base const& b) : elem{b} {}
-
-    constexpr Base& get() { return elem; }
-};
-#else
-#   warning inheritance-based MaybeEmpty is used
-template <class Base, bool Use_inheritance>
-class EBO_specialized : public Base {
-public:
-    constexpr EBO_specialized(Base const& b) : Base{b} {}
-    constexpr Base& get() { return *static_cast<Base*>(this); }
-};
-
-template <class Base>
-class EBO_specialized<Base, false> {
-public:
-    Base elem;
-    constexpr EBO_specialized(Base const& b) : elem{b} {}
-
-    constexpr Base& get() { return elem; }
-};
-
-template <class Base>
-class MaybeEmpty : public EBO_specialized<Base, Type<Base>( is_empty && !is_final )> {
-using EBOBase = EBO_specialized<Base, Type<Base>( is_empty && !is_final )>;
-public:
-    constexpr MaybeEmpty(Base const& d=Base()) : EBOBase{d} {}
-    using EBOBase::get;
-};
-#endif
 
 
 namespace detail {
@@ -109,8 +69,6 @@ public:
     ~ptr() noexcept { if (p) MaybeEmpty<Deleter>::get()(p); }
     
     explicit operator bool() { return p != nullptr; }
-
-    // operator view<T>() { return p; }
 
     T& operator* () { return view<T>::operator*(); }
     T* operator->() { return view<T>::operator->(); }
