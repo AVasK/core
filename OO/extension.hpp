07e776fb1efaf0254d0e0583253a812d45414276
Alex <avask.work@gmail.com>
2022-02-07 00:57:27 +0300

    Added SuperMixin to get uppermost base class in the mixin inheritance hierarchy when needed, removed using Super=Base from mixin macro

--- OO/extension.hpp
+++ OO/extension.hpp
@@ -10,11 +10,25 @@ using extend = chain<Base, Mixins...>;
 #define CORE_MIXIN(name, code)          \
 template <class Base>                   \
 struct name : public Base {             \
-    using Super = Base;                 \
     using Base::Base;                   \
     code                                \
 }                                       \
     
 
+namespace mixin {
+
+/**
+ * @brief provides ultimate Base class to the mixin hierarchy
+ *        via typedef `Super`
+ * @remark Should go first in the extension mixin list
+ * 
+ * @tparam Base 
+ */
+template <class Base>
+struct SuperMixin : public Base {
+    using Base::Base;
+    using Super = Base;
+};
+}//namespace mixin
 
 }//namespace core
