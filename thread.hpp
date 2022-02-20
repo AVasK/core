ef1d574182d195f06620b4128e59d0a623659282
Alex <avask.work@gmail.com>
2022-02-20 22:15:17 +0300

    Changed core::thread to use composition instead of inheritance
    Using inheritance gave some strange errors when passing core::thread into std::vector both via emplace_back and push_back(move(...))

--- thread.hpp
+++ thread.hpp
@@ -7,12 +7,41 @@ namespace core {
 /**
  * @brief Auto-joinable thread class
  */
-class thread : public std::thread {
-public:
-    using std::thread::thread; // using std::thread c'tors
+class thread {
+    using id = std::thread::id;
+    std::thread t;
+public:    
+
+    template <typename... Ts>
+    thread (Ts&&... args) noexcept(noexcept( std::thread(std::forward<Ts>(args)...) ))
+    : t{ std::forward<Ts>(args)... } {}
+
+    thread (thread const&) = delete;
+    thread& operator= (thread const&) = delete;
+
+    thread (thread && other) : t{ std::move(other.t) } {}
+    thread (std::thread && other) : t{ std::move(other) } {}
+
+    thread& operator= (thread && other) {
+        t = std::move(other.t);
+        return *this;
+    }
+
+    thread& operator= (std::thread && other) {
+        t = std::move(other);
+        return *this;
+    }
+
+    bool joinable() const noexcept { return t.joinable(); }
+    auto get_id() const noexcept -> std::thread::id { return t.get_id(); }
+    auto native_handle() { return t.native_handle(); }
+    static auto hardware_concurrency() noexcept { return std::thread::hardware_concurrency(); }
+
+    void join() { t.join(); }
+    void detach() { t.detach(); }
 
     ~thread(){
-        if ( joinable() ) { join(); }
+        if ( t.joinable() ) { t.join(); }
     }
 
 };
