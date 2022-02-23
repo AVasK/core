39a6b06b7fa723480238f565dca9772ad8778bfc
Alex <avask.work@gmail.com>
2022-02-24 00:23:41 +0300

    Added 2 types of locker proxies: one bare-bones with reference & minimal overhead and the moveable variation
    > RAII_locker - uses only RAII & reference
    
    > locked - uses pointer to enable move-construction and assignment
    
    + removed redundant `explicit` qualification

--- access.hpp
+++ access.hpp
@@ -10,11 +10,32 @@
 
 namespace core {
 
+// Stricter version of locker (similar to std::lock_guard)
+template <typename T, class Mutex=std::mutex>
+struct RAII_locker {
+    constexpr RAII_locker(T& obj, Mutex & mut) : ref{ obj }, m{ mut } { m.lock(); }
+    constexpr RAII_locker(T& obj, Mutex & mut, std::adopt_lock_t) : ref{ obj }, m{ mut } {/*adopting the locked mutex*/}
+    RAII_locker( RAII_locker&& other ) = delete;
+    RAII_locker( RAII_locker const& ) = delete;
+    RAII_locker& operator= (RAII_locker const& ) = delete;
+    RAII_locker& operator= (RAII_locker && other) = delete;
 
+    constexpr T const* operator->() const noexcept { return &ref; }
+    constexpr T* operator->() noexcept { return &ref; }
+    constexpr T& operator*() noexcept { return ref; } 
+    operator T& () noexcept { return ref; }
+    ~RAII_locker(){ m.unlock(); }
+private:
+    T & ref;
+    Mutex & m;
+};
+
+
+// Move-enabled RAII locking class (potential overhead: uses pointer vs reference and checks it in d-tor)
 template <typename T, class Mutex=std::mutex>
 struct locked {
-    explicit constexpr locked(T& obj, Mutex & mut) : ref{ obj }, m{ &mut } { m->lock(); }
-    explicit constexpr locked(T& obj, Mutex & mut, std::adopt_lock_t) : ref{ obj }, m{ &mut } {/*adopting the locked mutex*/}
+    constexpr locked(T& obj, Mutex & mut) : ref{ obj }, m{ &mut } { m->lock(); }
+    constexpr locked(T& obj, Mutex & mut, std::adopt_lock_t) : ref{ obj }, m{ &mut } {/*adopting the locked mutex*/}
     locked( locked&& other ) : ref{ other.ref }, m{ other.m } { other.m = nullptr; };
     locked( locked const& ) = delete;
     locked& operator= (locked const& ) = delete;
@@ -39,16 +60,16 @@ template <typename T, class Mutex=std::mutex>
 struct access {
     constexpr access (T const& v) : value{v}, _mutex{} {}
 
-    constexpr locked<T,Mutex> operator->() const noexcept { return locked<T,Mutex>(value, _mutex); }
-    constexpr locked<T,Mutex> operator->() noexcept { return locked<T,Mutex>(value, _mutex); }
+    constexpr RAII_locker<T,Mutex> operator->() const noexcept { return {value, _mutex}; }
+    constexpr RAII_locker<T,Mutex> operator->() noexcept { return {value, _mutex}; }
 
     constexpr Mutex mutex() { return _mutex; }
 
-    constexpr locked<T,Mutex> lock() { return locked<T,Mutex>(value, _mutex); }
+    constexpr locked<T,Mutex> lock() { return {value, _mutex}; }
     
     #if __cplusplus/100 >= 2017
     std::optional<locked<T,Mutex>> try_lock() { 
-        if (_mutex.try_lock()) { return locked<T,Mutex>(value, _mutex, std::adopt_lock); }
+        if (_mutex.try_lock()) { return {value, _mutex, std::adopt_lock}; }
         else return {};
     }
     #endif
