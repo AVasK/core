b777342073c376f4a11cd7d67b9d26e9f9053892
Alex <avask.work@gmail.com>
2022-02-24 00:06:15 +0300

    added try_lock, made locked<> more like std::unique_lock impl-wise
    Had to change a Mutex ref to pointer to allow move-construction & assignment.
    ( In some cases C++17's mandatory copy elision helped disguise it for some cases, but I guess better safe than UB :)

--- access.hpp
+++ access.hpp
@@ -4,41 +4,58 @@
 #include <mutex>
 #include <iostream> // temp
 
+#if __cplusplus/100 >= 2017
+#include <optional>
+#endif
+
 namespace core {
 
 
 template <typename T, class Mutex=std::mutex>
 struct locked {
-    explicit constexpr locked(T& obj, Mutex & mut) : ref{ obj }, m{ mut } { m.lock(); }
-    locked( locked&& other ) = default;
+    explicit constexpr locked(T& obj, Mutex & mut) : ref{ obj }, m{ &mut } { m->lock(); }
+    explicit constexpr locked(T& obj, Mutex & mut, std::adopt_lock_t) : ref{ obj }, m{ &mut } {/*adopting the locked mutex*/}
+    locked( locked&& other ) : ref{ other.ref }, m{ other.m } { other.m = nullptr; };
     locked( locked const& ) = delete;
     locked& operator= (locked const& ) = delete;
+    locked& operator= (locked && other) { 
+        ref = other.ref;  
+        m = other.m;
+        other.m = nullptr;
+    }
 
     constexpr T const* operator->() const noexcept { return &ref; }
     constexpr T* operator->() noexcept { return &ref; }
     constexpr T& operator*() noexcept { return ref; } 
     operator T& () noexcept { return ref; }
-    ~locked(){ m.unlock(); }
+    ~locked(){ if (m) m->unlock(); }
 private:
     T & ref;
-    Mutex & m;
+    Mutex * m;
 };
 
 
 template <typename T, class Mutex=std::mutex>
 struct access {
-    constexpr access (T const& v) : value{v}, mutex{} {}
-    // if Mutex is a reference type, then can re-link to another mutex?
-    // access (T const& v, Mutex other) : ...
+    constexpr access (T const& v) : value{v}, _mutex{} {}
 
-    constexpr locked<T,Mutex> operator->() const noexcept { return locked<T,Mutex>(value, mutex); }
-    constexpr locked<T,Mutex> operator->() noexcept { return locked<T,Mutex>(value, mutex); }
+    constexpr locked<T,Mutex> operator->() const noexcept { return locked<T,Mutex>(value, _mutex); }
+    constexpr locked<T,Mutex> operator->() noexcept { return locked<T,Mutex>(value, _mutex); }
 
-    constexpr locked<T,Mutex> lock() noexcept { return locked<T,Mutex>(value, mutex); }
+    constexpr Mutex mutex() { return _mutex; }
+
+    constexpr locked<T,Mutex> lock() { return locked<T,Mutex>(value, _mutex); }
+    
+    #if __cplusplus/100 >= 2017
+    std::optional<locked<T,Mutex>> try_lock() { 
+        if (_mutex.try_lock()) { return locked<T,Mutex>(value, _mutex, std::adopt_lock); }
+        else return {};
+    }
+    #endif
 
 private:
     T value;
-    Mutex mutex;
+    Mutex _mutex;
 };
 
 }// namespace core
