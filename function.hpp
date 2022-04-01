bbc7c018d94ac245f6c473b80b690ae97bb9926d
Alex <avask.work@gmail.com>
2022-04-01 15:35:28 +0300

    Added core::invoke for std>=C++14

--- function.hpp
+++ function.hpp
@@ -3,7 +3,37 @@
 #include <type_traits>
 #include "typesystem.hpp"
 
+namespace core {
 
+#if __cplusplus/100 >= 2017
+    using std::invoke;
+#else
+// invoke:
+template<
+    typename Functor, typename... Args,
+    meta::require<
+        Type<Functor>.decay()( is_member_pointer )
+    >* = nullptr
+>
+constexpr decltype(auto) invoke(Functor&& f, Args&&... args) 
+noexcept( noexcept(std::mem_fn(f)(std::forward<Args>(args)...)) )
+{ 
+    return std::mem_fn(f)(std::forward<Args>(args)...); 
+}
+   
+template<typename Functor, typename... Args,
+    meta::require<
+        Type<Functor>.decay()( !is_member_pointer )
+    >* = nullptr
+>
+constexpr decltype(auto) invoke(Functor&& f, Args&&... args) 
+noexcept( noexcept(std::forward<Functor>(f)(std::forward<Args>(args)...)) )
+{ 
+    return std::forward<Functor>(f)(std::forward<Args>(args)...); 
+}
+#endif
+
+// Function reference (non-owning)
 template <typename Signature>
 class func_ref;
 
@@ -17,9 +47,11 @@ public:
     template <typename Callable,
         typename = std::enable_if_t< core::Type<Callable>.remove_ref() != core::Type<func_ref> >
     >
+    #if __cplusplus/100 >= 2020
     requires( core::Type<Callable>.remove_ref() != core::Type<func_ref> )
+    #endif
     func_ref(Callable && f) 
-    : fptr {reinterpret_cast<void*>( &f )}
+    : fptr {static_cast<void *>( &f )}
     , invoke_func{ [](void * fp, Args&&... args)-> Ret { return (*reinterpret_cast<Callable*>(fp)) (std::forward<Args>(args)...); } } 
     {}
 
@@ -30,3 +62,5 @@ public:
 private: 
     void * fp = nullptr;
 };
+
+}//namespace core
