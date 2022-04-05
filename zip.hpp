d383b406caf62355a43f9009bb388834c1de614e
Alex <avask.work@gmail.com>
2022-04-05 21:09:37 +0300

    [fix] enumerate: Counter=size_t

--- zip.hpp
+++ zip.hpp
@@ -66,8 +66,8 @@ auto zip(T && t, U && u) {
     return zip_adaptor<T,U>(std::forward<T>(t), std::forward<U>(u));
 }
 
-template <typename Iterable, typename Counter>
-auto enumerate(Iterable && iterable, Counter start=(size_t)0 ) {
+template <typename Iterable, typename Counter=size_t>
+auto enumerate(Iterable && iterable, Counter start=0) {
     using IndexType = meta::select<std::numeric_limits<Counter>::is_signed, core::i64, size_t>;
     return zip_adaptor<Iterable, core::Range<IndexType>>(std::forward<Iterable>(iterable), core::range<IndexType>(start, std::numeric_limits<IndexType>::max()));
 }
