2878cb57307fcc7104339d0e942552e4e3ddd39e
Alex <avask.work@gmail.com>
2022-04-22 01:35:07 +0300

    foreach -> apply

--- apply.hpp
+++ apply.hpp
@@ -3,13 +3,14 @@
 #include <utility>
 #include "typesystem/typelist.hpp"
 #include "meta.hpp"
+#include "traits/iter_traits.hpp"
 
 namespace core {
 
 namespace detail {
         
     template <typename F>
-    struct elementwise_apply {
+    struct fmap {
         F f;
     };
 
@@ -29,17 +30,36 @@ namespace detail {
         }
     };
 
+    // Tuple
     template <typename F, typename... Ts>
-    constexpr decltype(auto) operator|(std::tuple<Ts...> const& t, elementwise_apply<F> && op) {
+    constexpr decltype(auto) operator| (std::tuple<Ts...> const& t, fmap<F> && op) {
         constexpr bool all_void = core::Types<decltype( std::move(op).f(std::declval<Ts>()) )...>.all( core::is_void );
         using R = meta::select<all_void, void, std::tuple<decltype(std::move(op).f(std::declval<Ts>()))...>>;
         return tuple_map<R>::impl(std::make_index_sequence<sizeof...(Ts)>{}, std::move(op).f, t);
     }
 
+    // Iterable
+    template <typename F, typename Iterable,
+        typename=meta::require< core::is_iterable<Iterable>{} >
+    >
+    constexpr decltype(auto) operator| (Iterable&& iter, fmap<F> && map) {
+        using T = decltype( *std::begin(iter) );
+        using NewT = decltype(std::move(map).f(std::declval< T >()));
+        using NewIterable = meta::unpack_base<std::remove_reference_t<Iterable>, NewT>;
+
+        NewIterable ret = std::forward<Iterable>( iter );
+
+        for (auto& v : ret) {
+            v = std::move(map).f( v );
+        }
+
+        return ret;
+    } 
+
 }//namespace detail
 
 template <typename F>
-constexpr auto foreach(F && f) -> detail::elementwise_apply<F> { return {std::forward<F>(f)}; }
+constexpr auto apply(F && f) -> detail::fmap<F> { return {std::forward<F>(f)}; }
 
 // template <typename F, typename T, size_t... Is>
 // constexpr decltype(auto) tuple_map(std::index_sequence<Is...>, F && f, T&& tuple) {
