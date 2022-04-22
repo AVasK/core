a97dff7aefefe6c70d5eb24b5eae9f1ab1e2d27b
Alex <avask.work@gmail.com>
2022-04-22 17:26:37 +0300

    bugfix in tuple, added support for Iterables

--- apply.hpp
+++ apply.hpp
@@ -2,8 +2,10 @@
 
 #include <utility>
 #include "typesystem/typelist.hpp"
+#include "typesystem/type_manipulation.hpp"
 #include "meta.hpp"
 #include "traits/iter_traits.hpp"
+#include "iteration.hpp"
 
 namespace core {
 
@@ -14,6 +16,7 @@ namespace detail {
         F f;
     };
 
+    // =====[ Tuple ]=====
     template <typename R>
     struct tuple_map {
         template <typename F, typename T, size_t... Is>
@@ -32,32 +35,99 @@ namespace detail {
 
     // Tuple
     template <typename F, typename... Ts>
-    constexpr decltype(auto) operator| (std::tuple<Ts...> const& t, fmap<F> && op) {
-        constexpr bool all_void = core::Types<decltype( std::move(op).f(std::declval<Ts>()) )...>.all( core::is_void );
-        using R = meta::select<all_void, void, std::tuple<decltype(std::move(op).f(std::declval<Ts>()))...>>;
+    constexpr decltype(auto) operator| (std::tuple<Ts...> & t, fmap<F> && op) {
+        constexpr bool all_void = core::Types<decltype( std::move(op).f(std::declval<Ts&>()) )...>.all( core::is_void );
+        using R = meta::select<all_void, void, std::tuple<decltype(std::move(op).f(std::declval<Ts&>()))...>>;
         return tuple_map<R>::impl(std::make_index_sequence<sizeof...(Ts)>{}, std::move(op).f, t);
     }
 
-    // Iterable
+    template <typename F, typename... Ts>
+    constexpr decltype(auto) operator| (std::tuple<Ts...> const& t, fmap<F> && op) {
+        constexpr bool all_void = core::Types<decltype( std::move(op).f(std::declval<Ts const&>()) )...>.all( core::is_void );
+        using R = meta::select<all_void, void, std::tuple<decltype(std::move(op).f(std::declval<Ts const&>()))...>>;
+        return tuple_map<R>::impl(std::make_index_sequence<sizeof...(Ts)>{}, std::move(op).f, t);
+    }
+
+    template <typename F, typename... Ts>
+    constexpr decltype(auto) operator| (std::tuple<Ts...> && t, fmap<F> && op) {
+        constexpr bool all_void = core::Types<decltype( std::move(op).f(std::declval<Ts&&>()) )...>.all( core::is_void );
+        using R = meta::select<all_void, void, std::tuple<decltype(std::move(op).f(std::declval<Ts&&>()))...>>;
+        return tuple_map<R>::impl(std::make_index_sequence<sizeof...(Ts)>{}, std::move(op).f, std::move(t));
+    }
+
+
+
+    // =====[ Iterable ]======
+
+    template <typename R>
+    struct iterable_map {
+        template <typename F, typename Iterable>
+        static constexpr R impl(Iterable&& iter, F && f) {
+            R ret ( iter.size() );
+            for (auto && [res_elem, iter_elem] : core::zip(ret, iter)) {
+                res_elem = std::move(f)( iter_elem );
+            }
+            return ret;
+        }
+    };
+
+    template <>
+    struct iterable_map<void> {
+        template <typename F, typename Iterable>
+        static constexpr void impl(Iterable&& iter, F && f) {
+            for (auto & elem : iter) std::move(f)( elem );
+        }
+    };
+
+
     template <typename F, typename Iterable,
         typename=meta::require< core::is_iterable<Iterable>{} >
     >
     constexpr decltype(auto) operator| (Iterable&& iter, fmap<F> && map) {
         using T = decltype( *std::begin(iter) );
         using NewT = decltype(std::move(map).f(std::declval< T >()));
-        using NewIterable = meta::unpack_base<std::remove_reference_t<Iterable>, NewT>;
+        using NewIterable = meta::unpack_base<core::remove_cvref_t<Iterable>, NewT>;
 
-        NewIterable ret = std::forward<Iterable>( iter );
+        using R = meta::select<(core::Type<NewT> != core::Type<void>), 
+            NewIterable,
+            void
+        >;
 
-        for (auto& v : ret) {
-            v = std::move(map).f( v );
-        }
+        return iterable_map<R>::impl(iter, std::move(map).f);
+    }
 
-        return ret;
-    } 
+    // C++17:
+    // template <typename F, typename Iterable,
+    //     typename=meta::require< core::is_iterable<Iterable>{} >
+    // >
+    // constexpr decltype(auto) operator| (Iterable&& iter, fmap<F> && map) {
+    //     using T = decltype( *std::begin(iter) );
+    //     using NewT = decltype(std::move(map).f(std::declval< T >()));
+    //     using NewIterable = meta::unpack_base<std::remove_reference_t<Iterable>, NewT>;
+    //     if constexpr (core::Type<NewT> != core::Type<void>) {
+    //         NewIterable ret ( iter.size() );
+    //         for (auto && [res_elem, iter_elem] : core::zip(ret, iter)) {
+    //             res_elem = std::move(map).f( iter_elem );
+    //         }
+    //         return ret;
+    //     }
+    //     else {
+    //         for (auto & elem : iter) {
+    //             std::move(map).f( elem );
+    //         }
+    //     }
+    // }
 
 }//namespace detail
 
+/**
+ * @brief apply: creates an apply operation object.
+ * `apply` operation creates new object if F returns value, otherwise transforms passed object in place
+ * 
+ * @tparam F 
+ * @param f 
+ * @return detail::fmap<F> 
+ */
 template <typename F>
 constexpr auto apply(F && f) -> detail::fmap<F> { return {std::forward<F>(f)}; }
 
